d94aebc6a5dfeeb88ff885b490d007d350f683c2
Gabriel Sena <gabrielsena8585@gmail.com>
2024-10-25 19:04:51 -0300

    Add files via upload

---
+++ programação orientada a objetos/programação orientada a objetos/SistemaBiblioteca/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaBiblioteca
+{
+    public class Usuario : Entidade
+    {
+        public string Nome { get; set; } = "Novo Usuário";
+        public string Telefone { get; set; } = "";
+        public string Endereco { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool Banido { get; set; } = false;
+    }
+}
